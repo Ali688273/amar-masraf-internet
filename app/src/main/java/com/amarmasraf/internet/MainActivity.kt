9160c028d59d38b1586570f84b7a02c1939a7e6e
Ali688273 <iiii688273@gmail.com>
2026-08-20 09:52:33 +0330

    Update MainActivity.kt

--- app/src/main/java/com/amarmasraf/internet/MainActivity.kt
+++ app/src/main/java/com/amarmasraf/internet/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import ir.tapsell.plus.TapsellPlus
-import ir.tapsell.plus.TapsellPlusInitListener
-import ir.tapsell.plus.model.AdNetworkError
-import ir.tapsell.plus.model.AdNetworks
-import ir.tapsell.plus.model.TapsellPlusAdModel
-import ir.tapsell.plus.model.TapsellPlusBannerType
-import ir.tapsell.plus.listener.AdRequestCallback
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,26 +17,5 @@ class MainActivity : AppCompatActivity() {
         webView.settings.domStorageEnabled = true
         webView.webViewClient = WebViewClient()
         webView.loadUrl("file:///android_asset/index.html")
-
-        TapsellPlus.initialize(this, "aanbcpksderreqsnknjnookaelpsgibjbrjbcfsicndoqmkimibmncsnfqrrbkccaiqnjb", object : TapsellPlusInitListener {
-            override fun onInitializeSuccess(adNetworks: AdNetworks) {
-                TapsellPlus.requestStandardBannerAd(
-                    this@MainActivity,
-                    "6a868ecdaf056d371d5ba541",
-                    TapsellPlusBannerType.BANNER_320x50,
-                    object : AdRequestCallback() {
-                        override fun response(tapsellPlusAdModel: TapsellPlusAdModel) {
-                            TapsellPlus.showStandardBannerAd(
-                                this@MainActivity,
-                                tapsellPlusAdModel.responseId,
-                                findViewById(android.R.id.content)
-                            )
-                        }
-                        override fun error(message: String) {}
-                    }
-                )
-            }
-            override fun onInitializeFailed(adNetworkError: AdNetworkError) {}
-        })
     }
 }
