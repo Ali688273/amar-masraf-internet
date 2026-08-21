126c648f17f4260637f96ac3323a98ddcad5646c
Ali688273 <iiii688273@gmail.com>
2026-08-21 08:33:28 +0330

    Update MainActivity.kt

--- app/src/main/java/com/amarmasraf/internet/MainActivity.kt
+++ app/src/main/java/com/amarmasraf/internet/MainActivity.kt
@@ -29,11 +29,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import ir.tapsell.plus.TapsellPlus
 import ir.tapsell.plus.TapsellPlusInitListener
-import ir.tapsell.plus.model.AdNetworkError
-import ir.tapsell.plus.model.AdNetworks
-import ir.tapsell.plus.model.TapsellPlusAdModel
-import ir.tapsell.plus.model.TapsellPlusBannerType
-import ir.tapsell.plus.listener.AdRequestCallback
+import ir.tapsell.plus.TapsellPlusBannerType
+import ir.tapsell.plus.AdRequestCallback
 import java.util.Calendar
 
 data class AppUsageInfo(
@@ -547,30 +544,32 @@ class MainActivity : AppCompatActivity() {
         return mode == AppOpsManager.MODE_ALLOWED
     }
 
-    // راه اندازی تپسل و نمایش بنر
+    // راه اندازی تپسل و نمایش بنر مطابق با SDK نسخه جدید
     private fun initAndShowTapsellAd(adContainerId: Int) {
         val tapsellKey = "aanbcpksderreqsnknjnookaelpsgibjbrjbcfsicndoqmkimibmncsnfqrrbkccaiqnjb"
         val zoneId = "6a868ecdaf056d371d5ba541"
 
         TapsellPlus.initialize(this, tapsellKey, object : TapsellPlusInitListener {
-            override fun onInitializeSuccess(adNetworks: AdNetworks) {
+            override fun onInitializeSuccess(adNetworks: Any?) {
                 TapsellPlus.requestStandardBannerAd(
                     this@MainActivity,
                     zoneId,
                     TapsellPlusBannerType.BANNER_320x50,
                     object : AdRequestCallback() {
-                        override fun response(tapsellPlusAdModel: TapsellPlusAdModel) {
-                            TapsellPlus.showStandardBannerAd(
-                                this@MainActivity,
-                                tapsellPlusAdModel.responseId,
-                                findViewById(adContainerId)
-                            )
+                        override fun response(responseId: String?) {
+                            if (responseId != null) {
+                                TapsellPlus.showStandardBannerAd(
+                                    this@MainActivity,
+                                    responseId,
+                                    findViewById(adContainerId)
+                                )
+                            }
                         }
-                        override fun error(message: String) {}
+                        override fun error(message: String?) {}
                     }
                 )
             }
-            override fun onInitializeFailed(adNetworkError: AdNetworkError) {}
+            override fun onInitializeFailed(error: String?) {}
         })
     }
 
